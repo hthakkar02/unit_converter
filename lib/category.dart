3b4a254f1473339b8221db9a03884497e1cf9920
Harsh Thakkar <thakkarharshemail@gmail.com>
2021-06-30 17:06:04 -0500

    Initial setup and list working

---
+++ lib/category.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+// We use an underscore to indicate that these variables are private.
+// See https://www.dartlang.org/guides/language/effective-dart/design#libraries
+final _rowHeight = 100.0;
+final _borderRadius = BorderRadius.circular(_rowHeight / 2);
+
+/// A custom [Category] widget.
+///
+/// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
+/// a colored [InkWell] animation.
+class Category extends StatelessWidget {
+  final String name;
+  final Color color;
+  final IconData iconLocation;
+
+  /// Creates a [Category].
+  ///
+  /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
+  /// the UI, and the icon that represents it (e.g. a ruler).
+  const Category({
+    required this.name,
+    required this.color,
+    required this.iconLocation,
+  });
+
+  /// Builds a custom widget that shows [Category] information.
+  ///
+  /// This information includes the icon, name, and color for the [Category].
+  @override
+  // This `context` parameter describes the location of this widget in the
+  // widget tree. It can be used for obtaining Theme data from the nearest
+  // Theme ancestor in the tree. Below, we obtain the display1 text theme.
+  // See https://docs.flutter.io/flutter/material/Theme-class.html
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        height: _rowHeight,
+        child: InkWell(
+          borderRadius: _borderRadius,
+          highlightColor: color,
+          splashColor: color,
+          // We can use either the () => function() or the () { function(); }
+          // syntax.
+          onTap: () => print('I was tapped!'),
+          child: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Icon(
+                    iconLocation,
+                    size: 60.0,
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    name,
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
